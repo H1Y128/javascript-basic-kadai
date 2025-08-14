469e941a4b859ef439c6a28c5310d1230b6a02e3
H1Y1128 <one6666yh@gmail.com>
2025-08-14 15:31:14 +0900

    課題20の修正（定数の位置を変更）

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById('btn');
 
+const text = document.getElementById('text');
+
 btn.addEventListener('click', () => {
-  const text = document.getElementById('text');
   text.textContent = 'ボタンをクリックしました'
 });
